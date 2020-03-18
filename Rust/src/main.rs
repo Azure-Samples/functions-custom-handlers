1303f68bfe38619ee1a7a9022874e8e6ea38b095
Ryan Levick <ryan.levick@gmail.com>
2020-03-18 11:58:07 +0100

    Clean up the Rust example

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,50 +1,40 @@
-extern crate hyper;
-extern crate futures;
-
-use std::env;
-use futures::future;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Error, Request, Response, Server};
 use hyper::{Method, StatusCode};
-use hyper::{Body, Request, Response, Server};
-use hyper::service::service_fn;
-use hyper::rt::{self, Future};
+use std::convert::Infallible;
+use std::env;
 
-//use std::{thread, time};
+async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let builder = Response::builder();
 
-type BoxFut = Box<dyn Future<Item=Response<Body>, Error=hyper::Error> + Send>;
-
-fn handler(req: Request<Body>) -> BoxFut {
-    let mut response = Response::new(Body::empty());
-
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/SimpleHttpTrigger") => {
-            *response.body_mut() = Body::from("Hello World from rust http worker");
-        },
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        },
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        },
+    let response = match (req.method(), req.uri().path()) {
+        (&Method::GET, "/SimpleHttpTrigger") => builder
+            .body(Body::from("Hello World from rust http worker"))
+            .unwrap(),
+        (&Method::POST, "/echo") => builder.body(req.into_body()).unwrap(),
+        _ => builder
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from(""))
+            .unwrap(),
     };
 
-    Box::new(future::ok(response))
+    Ok(response)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let port_key = "FUNCTIONS_HTTPWORKER_PORT";
-    let port: u16 = match env::var_os(port_key) {
-        Some(val) => val.into_string().unwrap().parse().expect("worker port is not a number!"),
-        None => 3000
+    let port: u16 = match env::var(port_key) {
+        Ok(val) => val.parse().expect("worker port is not a number!"),
+        Err(_) => 3000,
     };
     let addr = ([127, 0, 0, 1], port).into();
 
-    //thread::sleep(time::Duration::from_millis(20));
-
-    println!("Starting server on http://{}", addr);
-    let server = Server::bind(&addr)
-        .serve(|| service_fn(handler))
-        .map_err(|e| eprintln!("server error: {}", e));
+    let server = Server::bind(&addr).serve(make_service_fn(|_| async move {
+        Ok::<_, Infallible>(service_fn(handler))
+    }));
 
     println!("Listening on http://{}", addr);
-    rt::run(server);
+
+    server.await
 }
