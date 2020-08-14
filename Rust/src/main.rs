ee91da52a02c976651d27bba2eac8d8f6922cf5b
Shekhar <patnaikshekhar@gmail.com>
2020-08-14 07:57:18 +0000

    Rust - Added example for blob trigger

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,24 +1,51 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Error, Request, Response, Server};
 use hyper::{Method, StatusCode};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::convert::Infallible;
 use std::env;
 
 async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let builder = Response::builder();
-
     let response = match (req.method(), req.uri().path()) {
-        (&Method::GET, "/SimpleHttpTrigger") => builder
-            .body(Body::from("Hello World from rust http worker"))
-            .unwrap(),
-        (&Method::POST, "/echo") => builder.body(req.into_body()).unwrap(),
-        _ => builder
+        (&Method::GET, "/SimpleHttpTrigger") => http_trigger(req).await,
+        (&Method::POST, "/BlobTrigger") => blob_trigger(req).await,
+        _ => Response::builder()
             .status(StatusCode::NOT_FOUND)
             .body(Body::from(""))
             .unwrap(),
     };
 
     Ok(response)
+}
+
+async fn http_trigger(_req: Request<Body>) -> Response<Body> {
+    let builder = Response::builder();
+    builder
+        .body(Body::from("Hello World from rust http worker"))
+        .unwrap()
+}
+
+async fn blob_trigger(req: Request<Body>) -> Response<Body> {
+    let builder = Response::builder();
+    let b = hyper::body::to_bytes(req.into_body()).await.unwrap();
+    let r: FnRequest = serde_json::from_slice(&b.to_vec()).unwrap();
+    let base64_blob_contents = r.data.get("triggerBlob").unwrap();
+    let blob_contents = base64::decode(base64_blob_contents).unwrap();
+    let str_blob_contents = std::str::from_utf8(&blob_contents).unwrap();
+
+    println!("Contents {}", str_blob_contents);
+
+    let response = FnResponse {
+        outputs: None,
+        return_value: Some(str_blob_contents.to_string()),
+    };
+
+    builder
+        .status(200)
+        .header("Content-Type", "application/json")
+        .body(Body::from(serde_json::to_string(&response).unwrap()))
+        .unwrap()
 }
 
 #[tokio::main]
@@ -37,4 +64,20 @@ async fn main() -> Result<(), Error> {
     println!("Listening on http://{}", addr);
 
     server.await
+}
+
+#[derive(Debug, Deserialize)]
+struct FnRequest {
+    #[serde(rename = "Data")]
+    data: HashMap<String, String>,
+    #[serde(rename = "Metadata")]
+    metadata: HashMap<String, String>,
+}
+
+#[derive(Debug, Serialize)]
+struct FnResponse {
+    #[serde(rename = "Outputs")]
+    outputs: Option<HashMap<String, String>>,
+    #[serde(rename = "ReturnValue")]
+    return_value: Option<String>,
 }
